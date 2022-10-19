9a626c185b934f22d73d8a071a54439acd7a4d6e
centosei <centoseisigarette@gmail.com>
2022-10-19 01:50:44 +0000

    receive form data

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginPostRequest;
 
 class AuthController extends Controller
 {
@@ -14,6 +15,19 @@ class AuthController extends Controller
     public function index()
     {
         return view('index');
+    }
+    
+    /**
+     * 入力を受け取る
+     * 
+     * @return \Illuminate\View\View
+     */
+    public function login(LoginPostRequest $request)
+    {
+        $datum = $request->validated();
+        var_dump($datum); exit;
+        
+        // return view('test.input', ['datum' => $validatedData]);
     }
 }
 
